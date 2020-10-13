bb50ad1ccd6583463131de7214f85dbb22415c67
Peter Paul.Clemente <72077161+Peterpaulclemente@users.noreply.github.com>
2020-10-13 14:34:41 +0800

    Commit a message

---
+++ ADETQ3_PETER-PAUL_CLEMENTE/ADETQ3_PETER-PAUL_CLEMENTE/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ADETQ3_PETER_PAUL_CLEMENTE.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
